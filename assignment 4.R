0d316897b38183e330ea630e51e561b52cf1710e
Rise Sofie Pauline (sri041) <sri041@rstudio.uit.no>
2020-09-30 13:21:16 +0200

    Assignment 3

---
+++ assignment 4.R
@@ -0,0 +1,33 @@
+##Assignment 4
+library(tidyverse)
+library(rvest)
+
+
+##upload data 
+timeplan<- "http://timeplan.uit.no/emne_timeplan.php?sem=20h&module%5B%5D=BED-2056-1&View=list"
+
+scraped <- Sys.time()  ##when did download happen
+webpag <- read_html(timeplan)
+
+##checking if its working
+html_session("http://timeplan.uit.no/emne_timeplan.php?sem=20h&module%5B%5D=BED-2056-1&View=list")
+
+##there are 
+x<-html_table(html_nodes(webpag, "div table")[1:14])
+
+##making variables of the list 
+timeplan <- as.data.frame(matrix(unlist(x), nrow =14, byrow=T),stringsAsFactors=FALSE)
+
+timeplan %>% 
+  rownames_to_column() %>% 
+  gather(variable, value, -rowname) %>% 
+  spread(rowname, value) %>%
+  
+  
+  
+  view(timeplan)
+ 
+
+         
+
+
